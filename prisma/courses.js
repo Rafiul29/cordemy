a7bccce5b9a070f980e39f179643886af98db212
Rafiul29 <rafiulislam4472@gmail.com>
2023-05-01 11:02:30 +0600

    prisma added

---
+++ prisma/courses.js
@@ -0,0 +1,18 @@
+import prisma from "./prisma";
+
+//Get All courses
+export const getAllCourses=async()=>{
+  const courses=await prisma.course.findAll({})
+
+  return courses
+}
+
+//Get a single course
+export const getSingleCourse=async(id)=>{
+  const course= await prisma.course.findUnique(
+    {
+      where:{id}
+    }
+  )
+ return course
+}
