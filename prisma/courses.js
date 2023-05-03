388dbd72e695c220144976aa1d946d085cc1d539
Rafiul29 <rafiulislam4472@gmail.com>
2023-05-03 18:00:45 +0600

    course api done

--- prisma/courses.js
+++ prisma/courses.js
@@ -2,17 +2,15 @@ import prisma from "./prisma";
 
 //Get All courses
 export const getAllCourses=async()=>{
-  const courses=await prisma.course.findAll({})
+  const courses = await prisma.course.findMany({});
 
   return courses
 }
 
 //Get a single course
 export const getSingleCourse=async(id)=>{
-  const course= await prisma.course.findUnique(
-    {
+  const course= await prisma.course.findUnique({
       where:{id}
-    }
-  )
+    })
  return course
 }
