23c28705d2ce4d8daf4c9bbf0ddec9d4d91af094
Rafiul29 <rafiulislam4472@gmail.com>
2023-05-14 16:37:32 +0600

    course detail added

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,24 +1,39 @@
-import Link from 'next/link'
-import React from 'react'
-import Button from './Button'
+import Link from "next/link";
+import React from "react";
+import Button from "./Button";
 
 const Navbar = () => {
   return (
     <div className="h-20 bg-black text-gray-400 flex items-center">
-      <div className="wrapper flex justify-between">
-        <Link href="/" className='text-white font-semibold text-xl'>Codemy</Link>
-        <div className='flex gap-5'>
-        <Link href="/" className="hover:text-white transition-color">Home</Link>
-        <Link href="/courses" className="hover:text-white transition-color">Courses</Link>
-        <Link href="/about" className="hover:text-white transition-color">About</Link>
-        <Link href="/contact" className="hover:text-white transition-color">Contact</Link>
+      <div className="wrapper flex justify-between items-center">
+        <Link href="/" className="text-white font-semibold text-xl">
+          Codemy
+        </Link>
+        <div className="flex gap-5 ">
+          <Link href="/" className="hover:text-white transition-color">
+            Home
+          </Link>
+          <Link href="/courses" className="hover:text-white transition-color">
+            Courses
+          </Link>
+          <Link href="/about" className="hover:text-white transition-color">
+            About
+          </Link>
+          <Link href="/contact" className="hover:text-white transition-color">
+            Contact
+          </Link>
         </div>
         <div>
-      <Button href="/login" placeholder="Sign in" color="secondary" size="default"/>
+          <Button
+            href="/users/login"
+            placeholder="Sign in"
+            color="secondary"
+            size="default"
+          />
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
